e6ab459adfe5c79bceddce97649ee1595182c7a2
Denis Nesterov <https://github.com/exelure>
2023-04-08 19:23:57 +0500

    cards: initial setup
    Signed-off-by: Denis Nesterov <https://github.com/exelure>

---
+++ cards/src/main/kotlin/denis/nesterov/demo/microservices/cards/CardsApplication.kt
@@ -0,0 +1,13 @@
+package denis.nesterov.demo.microservices.cards
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@SpringBootApplication
+@EnableJpaRepositories
+class CardsApplication
+
+fun main(args: Array<String>) {
+	runApplication<CardsApplication>(*args)
+}
---
+++ cards/src/test/kotlin/denis/nesterov/demo/microservices/cards/CardsApplicationTests.kt
@@ -0,0 +1,13 @@
+package denis.nesterov.demo.microservices.cards
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
