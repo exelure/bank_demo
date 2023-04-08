ddb39952e6a1efc59f8a97bda3d773f852136108
Denis Nesterov <https://github.com/exelure>
2023-04-08 23:44:23 +0500

    configuration: initial setup of Spring Cloud config server
    Signed-off-by: Denis Nesterov <https://github.com/exelure>

---
+++ configuration/build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.7.10"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	kotlin("jvm") version "1.8.20"
+	kotlin("plugin.spring") version "1.8.20"
+}
+
+group = "denis.nesterov.demo.microservices"
+version = "1.0.0"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2021.0.6"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.cloud:spring-cloud-config-server")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+// disable plain jar
+tasks.named<Jar>("jar") {
+	enabled = false
+}
---
+++ configuration/src/test/kotlin/denis/nesterov/demo/microservices/configuration/ConfigurationApplicationTests.kt
@@ -0,0 +1,13 @@
+package denis.nesterov.demo.microservices.configuration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigurationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
