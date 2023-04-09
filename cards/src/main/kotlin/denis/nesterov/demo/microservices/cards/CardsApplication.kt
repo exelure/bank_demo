621c7593fbd723036ac41018ce93d213e9d40a04
Denis Nesterov <https://github.com/exelure>
2023-04-09 21:19:55 +0500

    bank_demo: added refresh scope of apps properties
    Signed-off-by: Denis Nesterov <https://github.com/exelure>

--- cards/src/main/kotlin/denis/nesterov/demo/microservices/cards/CardsApplication.kt
+++ cards/src/main/kotlin/denis/nesterov/demo/microservices/cards/CardsApplication.kt
@@ -3,11 +3,13 @@ package denis.nesterov.demo.microservices.cards
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @EnableJpaRepositories
 @ConfigurationPropertiesScan
+@RefreshScope
 class CardsApplication
 
 fun main(args: Array<String>) {
--- loans/src/main/kotlin/denis/nesterov/demo/microservices/loans/LoansApplication.kt
+++ loans/src/main/kotlin/denis/nesterov/demo/microservices/loans/LoansApplication.kt
@@ -3,11 +3,13 @@ package denis.nesterov.demo.microservices.loans
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @EnableJpaRepositories
 @ConfigurationPropertiesScan
+@RefreshScope
 class LoansApplication
 
 fun main(args: Array<String>) {
