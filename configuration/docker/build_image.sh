4a4c87b6a6928396b323e822033e35d6ed28cd2e
Denis Nesterov <https://github.com/exelure>
2023-04-09 20:23:21 +0500

    configuration: extended Dockerfile
    Signed-off-by: Denis Nesterov <https://github.com/exelure>

--- configuration/docker/build_image.sh
+++ configuration/docker/build_image.sh
@@ -1,4 +1,12 @@
 
 cd ..
 ./gradlew clean build
-docker build -t bank_demo/configuration -f docker/Dockerfile .
+docker build \
+  -t bank_demo/configuration \
+  -f docker/Dockerfile \
+  --build-arg SSH_CONFIGS_KEY="$(cat ~/.ssh/bank_demo_configs)" \
+  --build-arg SSH_KNOWN_HOSTS="$(ssh-keyscan -t rsa github.com)" \
+  --build-arg SSH_CONFIGS_HOST_NAME="bank_demo_configs_host" \
+  --build-arg SSH_CONFIGS_HOST_VALUE="github.com" \
+  --build-arg SSH_CONFIGS_USER="exelure" \
+  .
