be59930b18bf1d1798fd15ea562b10f92c08e4b2
Denis Nesterov <https://github.com/exelure>
2023-04-09 20:38:24 +0500

    configuration: added build images script
    Signed-off-by: Denis Nesterov <https://github.com/exelure>

---
+++ build_images.sh
@@ -0,0 +1,16 @@
+
+cd configuration/docker
+./build_image.sh
+cd ../..
+
+cd accounts/docker
+./build_image.sh
+cd ../..
+
+cd cards/docker
+./build_image.sh
+cd ../..
+
+cd loans/docker
+./build_image.sh
+cd ../..
