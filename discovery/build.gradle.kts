863a27e0add85aef891b27921a50a94bea6d9f92
Denis Nesterov <https://github.com/exelure>
2023-04-14 19:18:36 +0500

    discovery: migrated to spring 3, jvm 17, spring cloud 2022.0.x
    Signed-off-by: Denis Nesterov <https://github.com/exelure>

--- discovery/build.gradle.kts
+++ discovery/build.gradle.kts
@@ -1,27 +1,30 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.10"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	id("org.springframework.boot") version "3.0.5"
+	id("io.spring.dependency-management") version "1.1.0"
 	kotlin("jvm") version "1.8.20"
 	kotlin("plugin.spring") version "1.8.20"
 }
 
 group = "denis.nesterov.demo.microservices"
 version = "1.0.0"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
 }
 
-extra["springCloudVersion"] = "2021.0.6"
+extra["springCloudVersion"] = "2022.0.2"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
@@ -34,7 +37,7 @@ dependencyManagement {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+		jvmTarget = "17"
 	}
 }
 
